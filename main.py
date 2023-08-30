c77d3047a5c04dd4de3093477a1dc43a42ebfbe8
CodyGreer1 <143456594+CodyGreer1@users.noreply.github.com>
2023-08-30 15:10:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 def main():
-    workhours = int(input('Enter your work hours: '))
+    workhours = int(input('Enter your work hours: 50 '))
     reg_hours = 40
     reg_rate = 18.25
     ov_rate = 27.78
 
    ##################################################
-   # Code your program here
+overtime = workhours - reg_hours
+   print (overtime) 
+overtime_wage = overtime * ov_rate
+   print (overtime_wage)
+regular_wage = reg_hours * reg_rate
+   print (regular_wage)
+total_wage = regular_wage + overtime_wage
+   print (total_wage) 
    ##################################################
-    # overtime = workhours - reg_hours
-    # overtime_wage = overtime * ov_rate
-    # regular_wage = reg_hours * reg_rate
-    # total_wage = regular_wage + overtime_wage
 
     print(f"Regular hours: {reg_hours} Regular Charge: {regular_wage}")
     print(f"Overtime hours: {overtime} Overtime Charge: {overtime_wage:.2f}")
